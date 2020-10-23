883bca089bb21e495d0ea26bd8f9a3eb19b40ecb
Georgios Varvarigos <gvarvarigos96@outlook.com>
2020-10-23 17:59:07 +0300

    Thesis Commit 1

--- thesis/urls.py
+++ thesis/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path,include
-from helloworld.views import hello
+from django.urls import include, path
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('guestbook/',include('guestbook.urls')),
+    path('', include('pages.urls')),
+    path('/admin',admin.site.urls),
+
 ]
+
