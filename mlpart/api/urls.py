f38646bf392f6399156333e8c3382660c32c9dd9
Georgios Varvarigos <gvarvarigos96@outlook.com>
2021-01-20 12:38:16 +0200

    Thesis Commit 40

--- mlpart/api/urls.py
+++ mlpart/api/urls.py
@@ -1,6 +1,16 @@
 from .views import FileOKViewSet
 from rest_framework.routers import DefaultRouter
 
-router = DefaultRouter()
+
+from .views import EndpointViewSet
+from .views import MLAlgorithmViewSet
+from .views import MLAlgorithmStatusViewSet
+from .views import MLRequestViewSet
+
+router = DefaultRouter(trailing_slash=False)
 router.register(r'mlpart', FileOKViewSet)
+router.register(r"endpoints", EndpointViewSet, basename="endpoints")
+router.register(r"mlalgorithms", MLAlgorithmViewSet, basename="mlalgorithms")
+router.register(r"mlalgorithmstatuses", MLAlgorithmStatusViewSet, basename="mlalgorithmstatuses")
+router.register(r"mlrequests", MLRequestViewSet, basename="mlrequests")
 urlpatterns = router.urls
--- mlpart/views.py
+++ mlpart/views.py
@@ -1,23 +1,7 @@
 from django.shortcuts import render, HttpResponseRedirect, reverse
-from django.contrib import messages
+from rest_framework import mixins
 from .forms import UploadForm
-#
-# class Home(TemplateView):
-#     template_name = 'mlpart/home.html'
 
-# def upload(request):
-#     context={}
-#     if request.method == 'POST':
-#         uploaded_file = request.FILES['document']
-#         fs = FileSystemStorage()
-#         name=fs.save(uploaded_file.name, uploaded_file )
-#         context['url'] = fs.url(name)
-#         print(uploaded_file.name)
-#         print(uploaded_file.size)
-#     return render(request, 'mlpart/upload.html', context)
-#
-# def upload_file (request):
-#     return render(request,'mlpart/upload.html')
 
 def FileUploadView(request):
     if request.method == 'POST':
@@ -32,4 +16,7 @@ def FileUploadView(request):
             'form': form,
         }
     return render(request, 'mlpart/upload.html', context)
+
+
+
 
